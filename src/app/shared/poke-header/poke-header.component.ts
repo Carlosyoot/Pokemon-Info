572128037cf76d8e48fad44c3347cb5da869f8a5
carlosyoot <carlosdanielyoot@gmail.com>
2025-04-01 18:39:44 -0300

    Projeto Pokemon-Angular

---
+++ src/app/shared/poke-header/poke-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poke-header',
+  templateUrl: './poke-header.component.html',
+  styleUrls: ['./poke-header.component.scss'],
+  standalone:false
+
+})
+export class PokeHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
